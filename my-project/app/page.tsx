6faeb0d266e3cadeae38f730d31a4f022cec92c7
matthewkuria <mathewkwachira@gmail.com>
2024-04-17 21:55:21 +0300

    some changes

---
+++ my-project/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div className="bg-blue h-full w-full">
+      <h1 className="font-bold underline text-lg text-red-600">
+      Hello Matt!
+    </h1>
+  </div>
+      );
+}
