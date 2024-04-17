23a4d10f7f42a4c695183b143b24824176e2fd60
matthewkuria <mathewkwachira@gmail.com>
2024-04-17 22:23:45 +0300

    add on submit to the form

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -5,14 +5,22 @@ import Image from "next/image";
 
 export default function Home() {
   const [inputVal, setInputVal] = useState("")
+  console.log(inputVal)
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+  }
   return (
   <div className=" h-full w-full">
 
     <div className="">
       <h1>Enter Your Name</h1>
     </div>
-    <form action="">
-      <input type="text" placeholder="Type Your Name..." />
+    <form action="" onSubmit={handleSubmit}>
+      <input type="text"
+       placeholder="Type Your Name..."
+       value={inputVal}
+       onChange={(e)=> setInputVal(e.target.value)}
+       />
       <button type="submit">Predict Data</button>
     </form>
   </div>
