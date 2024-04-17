cced6ce4159c3a4ce84cc9a2cb193c0e6ff3bb2e
matthewkuria <mathewkwachira@gmail.com>
2024-04-17 22:15:07 +0300

    add a submit button

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <div className="bg-black h-full w-full">
+  <div className=" h-full w-full">
 
     <div className="">
       <h1>Enter Your Name</h1>
     </div>
     <form action="">
       <input type="text" placeholder="Type Your Name..." />
+      <button type="submit">Predict Data</button>
     </form>
   </div>
       );
