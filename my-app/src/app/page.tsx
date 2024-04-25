35b67246259d899f1204dd8203cb3676df1ea3c7
matthewkuria <mathewkwachira@gmail.com>
2024-04-26 00:01:33 +0300

    Count when the button is clicked

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -4,11 +4,14 @@ import { useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(0)
+  const handleClick =()=>{
+    setCount((prevCount) => prevCount +1)
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <h1 className="text-green-500">Hello Matt!</h1>
      <p className="">{count}</p>
-     <Button>Click Me</Button>
+     <Button >Click Me</Button>
     </main>
   );
 }
