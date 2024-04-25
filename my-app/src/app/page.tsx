91551dc5603d59f7aa8f04af7793276841116718
matthewkuria <mathewkwachira@gmail.com>
2024-04-25 23:58:55 +0300

    add use client to my code

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,10 +1,14 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function Home() {
+  const [count, setCount] = useState(0)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <h1 className="text-green-500">Hello Matt!</h1>
-     <Button />
+     <p className="">{count}</p>
+     <Button>Click Me</Button>
     </main>
   );
 }
