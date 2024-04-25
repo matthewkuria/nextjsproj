703829f4390c32db2baf5f08cdc1f7d090ff3dc7
matthewkuria <mathewkwachira@gmail.com>
2024-04-25 23:55:58 +0300

    add the button to the page

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <h1 className="text-green-500">Hello Matt!</h1>
+     <Button />
     </main>
   );
 }
