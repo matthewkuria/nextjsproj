e51cd1957d44e8876e42ca6eced7213deae0ea02
matthewkuria <mathewkwachira@gmail.com>
2024-04-26 00:23:27 +0300

    add card to the page

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,5 +1,10 @@
 "use client";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { useState } from "react";
 
 export default function Home() {
@@ -9,9 +14,53 @@ export default function Home() {
   }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1 className="text-green-500">Matthew Kuria</h1>
-     <p className="">{count}</p>
-     <Button onClick={handleClick} >Click Me</Button>
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Edit user details</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Full Name</Label>
+              <Input id="name" placeholder="Name of your project" />
+              <Label htmlFor="name">Email</Label>
+              <Input id="name" placeholder="Name of your project" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="framework">Framework</Label>
+              <Select>
+                <SelectTrigger id="framework">
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  <SelectItem value="admin">Admin</SelectItem>
+                  <SelectItem value="user">User</SelectItem>
+                  
+                </SelectContent>
+              </Select>
+              <Label htmlFor="framework">Status</Label>
+              <Select>
+                <SelectTrigger id="framework">
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  <SelectItem value="admin">Active </SelectItem>
+                  <SelectItem value="user">Inactive</SelectItem>
+                  
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-end">
+       
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
+
     </main>
   );
 }
